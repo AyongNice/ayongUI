535ffec9cff5e7896e49e842c534a3691840e0b5
AyongNice <AyongNice@163.com>
2023-10-28 22:48:30 +0800

    添加路由重定向

--- components/button/index.jsx
+++ components/button/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import './index.css'
 //封装Button组件
-export default function Button(props) {
+export default function Button(props = {}) {
     const {type, size, disabled, text, onClick} = props;
     return (
         <button className={`ayong-btn ayong-btn-${type} ayong-btn-${size}`} disabled={disabled} onClick={onClick}>
