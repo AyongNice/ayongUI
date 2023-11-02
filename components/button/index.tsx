e7c52484d57b8edbeec5b0e47953e2ae2e59a72c
AyongNice <AyongNice@163.com>
2023-11-02 10:03:52 +0800

    1 、button 组件添加 参数枚举样式 默认大小 .default .small .large

--- components/button/index.tsx
+++ components/button/index.tsx
@@ -11,8 +11,8 @@ import styles from '../../config/style.module.less';
 export default function Button(props: ButtonProps = {}) {
     const {type, size, children, className = '', disabled, text, onClick} = props;
 
-    //宽高样式
-    const combinedClassName = `${but.ayongBtn} ${className}`;
+    //默认使用组件classname  参数className 覆盖默认样式
+    const combinedClassName = `${but.ayongBtn} ${className} ${but[size]}`;
     return (
         <button className={combinedClassName} disabled={disabled} onClick={onClick}>
             {children}
