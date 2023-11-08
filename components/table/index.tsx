78a47bf5022a1c4ce172227b7b42ce0344f18b34
AyongNice <AyongNice@163.com>
2023-11-08 19:56:40 +0800

    1: 添加 Table组件 行列 可拖拽功能

--- components/table/index.tsx
+++ components/table/index.tsx
@@ -1,17 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
 import table from './index.module.less'
 import {TableProps, Column, DataItem} from "./table";
 
 // console.log(table)
 
 function Table({columns, children, data, className}: TableProps) {
-    let tableColumns: Column[] = [];
+
+    let _tableColumns: Column[] = [];
 
     // 从props传递的columns或者通过<Table.Column>定义的列都可以使用
     if (columns) {
-        tableColumns = columns;
+        _tableColumns = columns;
     } else if (children) {
-        tableColumns = React.Children.map(children, (child) => {
+        _tableColumns = React.Children.map(children, (child) => {
             if (child.type.name === 'Column') {
                 return {
                     title: child.props.title,
@@ -22,22 +23,66 @@ function Table({columns, children, data, className}: TableProps) {
             }
         });
     }
+    const [tableColumns, setTableColumns] = useState<Column[]>(_tableColumns);
+    const [tableData, setTableData] = useState<DataItem[]>(data as DataItem[]);
+
+    const handleDragStart = (e: React.DragEvent<HTMLTableRowElement>, index: number): void => {
+        e.dataTransfer.setData('sourceIndex', index);
+    };
+    const handleDragStartData = (e: React.DragEvent<HTMLTableRowElement>, index: number): void => {
+        e.dataTransfer.setData('sourceIndexData', index);
+    };
+
+    const handleDragOver = (e: React.DragEvent<HTMLTableRowElement>, index: number): void => {
+        e.preventDefault();
+    };
+
+    const handleDrop = (e: React.DragEvent<HTMLTableRowElement>, index: number): void => {
+        const sourceIndex: number = parseInt(e.dataTransfer.getData('sourceIndex'));
+        const newColumns: Column[] = [...tableColumns];
+        const [movedColumn] = newColumns.splice(sourceIndex, 1);
+        newColumns.splice(index, 0, movedColumn);
+        setTableColumns(newColumns);
+    };
+
+    const handleDropData = (e: React.DragEvent<HTMLTableRowElement>, index: number): void => {
+        const sourceIndex: number = parseInt(e.dataTransfer.getData('sourceIndexData'));
+        const newTableData: DataItem[] = [...tableData];
+        const [movedTableData] = newTableData.splice(sourceIndex, 1);
+        newTableData.splice(index, 0, movedTableData);
+        setTableData(newTableData);
+    };
+
     const styleClassName: string = `${table.table} ${className} `;
 
     return (
         <table className={styleClassName}>
             <thead>
             <tr>
-                {tableColumns.map((column: Column) => (
-                    <th key={column.key}>{column.title}</th>
+                {tableColumns.map((column: Column, index: number) => (
+                    <th
+                        key={column.key}
+                        draggable
+                        onDragStart={(e: React.DragEvent<HTMLTableRowElement>,) => handleDragStart(e, index)}
+                        onDragOver={(e: React.DragEvent<HTMLTableRowElement>,) => handleDragOver(e, index)}
+                        onDrop={(e: React.DragEvent<HTMLTableRowElement>,) => handleDrop(e, index)}
+                    >
+                        {column.title}
+                    </th>
                 ))}
             </tr>
             </thead>
             <tbody>
-            {data.map((item: DataItem) => (
-                <tr key={item.key}>
+            {tableData.map((item: DataItem, index: number) => (
+                <tr key={item.key}
+                    draggable
+                    onDragStart={(e: React.DragEvent<HTMLTableRowElement>,) => handleDragStartData(e, index)}
+                    onDragOver={(e: React.DragEvent<HTMLTableRowElement>,) => handleDragOver(e, index)}
+                    onDrop={(e: React.DragEvent<HTMLTableRowElement>,) => handleDropData(e, index)}
+                >
                     {tableColumns.map((column: Column) => (
-                        <td key={column.key}>
+                        <td key={column.key}
+                        >
                             {column.render ? column.render(item[column.dataIndex], item) : item[column.dataIndex]}
                         </td>
                     ))}
