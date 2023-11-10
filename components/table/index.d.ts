ae4ac7e46655f2d3ea0312040fe36d9149869ee7
AyongNice <AyongNice@163.com>
2023-11-11 03:10:30 +0800

    1:表格新增表头分组功能

--- components/table/index.d.ts
+++ components/table/index.d.ts
@@ -2,35 +2,42 @@ import React, {ReactNode} from 'react';
 
 
 // 定义数据项的类型
-interface DataItem {
+export interface DataItem {
     key: string;
-    age: number;
-    address: string;
-    tags: string[];
+    [propName: string]: any;
 }
 
 // 定义列的类型
-interface Column {
+export interface Column {
     key: string;
     title: string;
     dataIndex: string;
     render?: (data: any, record: DataItem) => ReactNode;
 }
 
+export interface ColumnGroup {
+    key: string;
+    title: string;
+    children: Column[];
+}
+
 // 定义Table组件的属性类型
-interface TableProps {
+export interface TableProps {
     className?: string;// 样式类名
     data?: DataItem[];// 数据
     columns?: Column[];// 列数据
+    draggable?: boolean;// 是否可拖拽
+    onDdragAfter?: (data: DataItem[], column: Column[]) => void;// 拖拽后的回调
 }
 
 // 定义Column组件的属性类型
-interface ColumnProps {
+export interface ColumnProps {
     title: string;
     dataIndex: string;
     render?: (data: any, record: DataItem) => ReactNode;
     key?: string;
 }
+
 // declare const Table: React.FC<TableProps>;
 // declare const  Column: React.FC<ColumnProps>;
 // export default Table;
--- components/table/index.tsx
+++ components/table/index.tsx
@@ -1,10 +1,26 @@
 import React, {useState} from "react";
 import table from './index.module.less'
-import {TableProps, Column, DataItem} from "./index";
+import {TableProps, Column, DataItem, ColumnGroup} from "./index";
+import ConditionalRender from "../conditional-render/conditional-render.tsx";
 
 // console.log(table)
+function renderSubHeaders(columns: Column[]) {
+    console.log('columns', columns)
+    return (
+        <>
+            {columns.map((column: Column) => (
+                <th key={column.key}>
+                    {column.title}
+                </th>
+            ))}
+        </>
+    );
+}
 
-function Table({columns, children, data, className}: TableProps) {
+function Table({
+                   columns, children, data, className, draggable = false, onDdragAfter = () => {
+    }
+               }: TableProps) {
 
     let _tableColumns: Column[] = [];
 
@@ -21,36 +37,69 @@ function Table({columns, children, data, className}: TableProps) {
                     key: child.key,
                 };
             }
+            if (child.type.name === 'ColumnGroup') {
+                // 递归处理 ColumnGroup 中的列信息
+                return {
+                    title: child.props.title,
+                    type: 'columnGroup',
+                    children: React.Children.map(child.props.children, (columnChild) => {
+                        if (columnChild.type.name === 'Column') {
+                            return {
+                                title: columnChild.props.title,
+                                dataIndex: columnChild.props.dataIndex,
+                                render: columnChild.props.render,
+                                key: columnChild.key,
+                            };
+                        }
+                        return null;
+                    }).filter(Boolean), // 过滤掉 null
+                    key: child.key,
+                };
+            }
         });
     }
     const [tableColumns, setTableColumns] = useState<Column[]>(_tableColumns);
     const [tableData, setTableData] = useState<DataItem[]>(data as DataItem[]);
-
+    const [activeTR, setActiveTR] = useState<null | number>(null);
+    const [activeTD, setActiveTD] = useState<null | string>(null);
+    console.log('tableColumns', tableColumns)
     const handleDragStart = (e: React.DragEvent<HTMLTableRowElement>, index: number): void => {
-        e.dataTransfer.setData('sourceIndex', index);
+        if (!draggable) return;
+        setActiveTD(tableColumns[index].dataIndex);
+        e.dataTransfer.setData('sourceIndex', String(index));
     };
     const handleDragStartData = (e: React.DragEvent<HTMLTableRowElement>, index: number): void => {
-        e.dataTransfer.setData('sourceIndexData', index);
+        if (!draggable) return;
+        setActiveTR(index);
+        e.dataTransfer.setData('sourceIndexData', String(index));
     };
 
     const handleDragOver = (e: React.DragEvent<HTMLTableRowElement>, index: number): void => {
+        if (!draggable) return;
         e.preventDefault();
+
     };
 
     const handleDrop = (e: React.DragEvent<HTMLTableRowElement>, index: number): void => {
+        if (!draggable) return;
+        setActiveTD(null);
         const sourceIndex: number = parseInt(e.dataTransfer.getData('sourceIndex'));
         const newColumns: Column[] = [...tableColumns];
         const [movedColumn] = newColumns.splice(sourceIndex, 1);
         newColumns.splice(index, 0, movedColumn);
         setTableColumns(newColumns);
+        onDdragAfter(newColumns, tableData)
     };
 
     const handleDropData = (e: React.DragEvent<HTMLTableRowElement>, index: number): void => {
+        if (!draggable) return;
+        setActiveTR(null);
         const sourceIndex: number = parseInt(e.dataTransfer.getData('sourceIndexData'));
         const newTableData: DataItem[] = [...tableData];
         const [movedTableData] = newTableData.splice(sourceIndex, 1);
         newTableData.splice(index, 0, movedTableData);
         setTableData(newTableData);
+        onDdragAfter(newTableData, tableColumns)
     };
 
     const styleClassName: string = `${table.table} ${className} `;
@@ -59,35 +108,93 @@ function Table({columns, children, data, className}: TableProps) {
         <table className={styleClassName}>
             <thead>
             <tr>
-                {tableColumns.map((column: Column, index: number) => (
-                    <th
-                        key={column.key}
-                        draggable
-                        onDragStart={(e: React.DragEvent<HTMLTableRowElement>,) => handleDragStart(e, index)}
-                        onDragOver={(e: React.DragEvent<HTMLTableRowElement>,) => handleDragOver(e, index)}
-                        onDrop={(e: React.DragEvent<HTMLTableRowElement>,) => handleDrop(e, index)}
-                    >
-                        {column.title}
-                    </th>
+                {tableColumns.map((column: Column | ColumnGroup, index: number) => (
+                    <React.Fragment key={column.key}>
+                        {column.type === 'columnGroup' && (
+                            // 如果是列分组，则递归渲染子级表头
+                            <>
+                                <th colSpan={(column as ColumnGroup).children.length}>
+                                    {column.title}
+                                </th>
+                            </>
+                        )}
+                    </React.Fragment>
                 ))}
             </tr>
+            <tr>
+                {tableColumns.map((column: Column | ColumnGroup, index: number) => (
+                    <React.Fragment key={column.key}>
+                        {column.type === 'columnGroup' ?
+                            // 如果是列分组，则递归渲染子级表头
+                            <React.Fragment>
+                                {renderSubHeaders((column as ColumnGroup).children, (column as ColumnGroup).children.length)}
+                            </React.Fragment>
+                            : <th
+                                draggable={draggable}
+                                onDragStart={(e) => handleDragStart(e, index)}
+                                onDragOver={(e) => handleDragOver(e, index)}
+                                onDrop={(e) => handleDrop(e, index)}
+                            >
+                                {column.title}
+                            </th>}
+
+
+                    </React.Fragment>
+                ))}
+            </tr>
+
             </thead>
             <tbody>
-            {tableData.map((item: DataItem, index: number) => (
-                <tr key={item.key}
-                    draggable
-                    onDragStart={(e: React.DragEvent<HTMLTableRowElement>,) => handleDragStartData(e, index)}
-                    onDragOver={(e: React.DragEvent<HTMLTableRowElement>,) => handleDragOver(e, index)}
-                    onDrop={(e: React.DragEvent<HTMLTableRowElement>,) => handleDropData(e, index)}
-                >
-                    {tableColumns.map((column: Column) => (
-                        <td key={column.key}
+            <ConditionalRender mode='if' show={tableData.length !== 0}>
+                <ConditionalRender mode='if' show={tableData.length !== 0}>
+                    {tableData.map((item: DataItem, index: number) => (
+                        <tr
+                            key={item.key}
+                            draggable={draggable}
+                            className={activeTR === index ? table.aticve : ''}
+                            onDragStart={(e: React.DragEvent<HTMLTableRowElement>,) => handleDragStartData(e, index)}
+                            onDragOver={(e: React.DragEvent<HTMLTableRowElement>,) => handleDragOver(e, index)}
+                            onDrop={(e: React.DragEvent<HTMLTableRowElement>,) => handleDropData(e, index)}
                         >
-                            {column.render ? column.render(item[column.dataIndex], item) : item[column.dataIndex]}
-                        </td>
+                            {tableColumns.map((column: Column | ColumnGroup) => (
+                                <React.Fragment key={column.key}>
+                                    {column.type === 'columnGroup' ? (
+                                        // 如果是列分组，则递归渲染子列
+                                        <>
+                                            {column.children.map((subColumn: Column) => (
+                                                <td
+                                                    key={subColumn.key}
+                                                    className={activeTD === subColumn.dataIndex ? table.aticve : ''}
+                                                >
+                                                    {subColumn.render ? subColumn.render(item[subColumn.dataIndex], item) : item[subColumn.dataIndex]}
+                                                </td>
+                                            ))}
+                                        </>
+                                    ) : (
+                                        // 如果是单独的列，则渲染一个单独的表格单元格
+                                        <td
+                                            key={column.key}
+                                            className={activeTD === column.dataIndex ? table.aticve : ''}
+                                        >
+                                            {column.render ? column.render(item[column.dataIndex], item) : item[column.dataIndex]}
+                                        </td>
+                                    )}
+                                </React.Fragment>
+                            ))}
+                        </tr>
                     ))}
+                </ConditionalRender>
+
+            </ConditionalRender>
+            <ConditionalRender mode='if' show={false}>
+                <tr>
+                    <td>
+                        暂无数据
+                    </td>
+
                 </tr>
-            ))}
+            </ConditionalRender>
+
             </tbody>
         </table>
     );
@@ -96,6 +203,12 @@ function Table({columns, children, data, className}: TableProps) {
 function Column({title, dataIndex, render}) {
     return null; // 列定义不需要在这里渲染
 }
+
+function ColumnGroup({title, children}) {
+    return null; // 列定义不需要在这里渲染
+}
+
+Table.ColumnGroup = ColumnGroup;
 
 Table.Column = Column;
 
