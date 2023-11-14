b437c6dffe9c4367fc7e3778d359f1ad5c05b63b
AyongNice <AyongNice@163.com>
2023-11-14 21:12:18 +0800

    1:radio组件增加类型文件引入

--- components/radio/radio.d.ts
+++ components/radio/radio.d.ts
@@ -1,13 +1,13 @@
 import React from "react";
 
 export interface RadioProps {
-  label: "";
-  val: "";
-  className?: string; // 自定义样式类名
-  checkedIcon?: React.JSX; // icon图标
-  disabled: boolean; // 禁用
-  size?: "primary" | "default" | "dashed"; // 大小
-  checked: boolean; // 是否选中
+    label: string;
+    val: string;
+    className?: string; // 自定义样式类名
+    checkedIcon?: React.JSX; // icon图标
+    disabled: boolean; // 禁用
+    size?: "primary" | "default" | "dashed"; // 大小
+    checked: boolean; // 是否选中
 }
 
 export function Radio(props: RadioProps): React.FC<Props>;
