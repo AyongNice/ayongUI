9024a665e81f167d2e5a635493cc7b03967d26ff
AyongNice <AyongNice@163.com>
2023-11-18 11:48:47 +0800

    1:更改table组件 命名空间类型提示

--- components/table/index.d.ts
+++ components/table/index.d.ts
@@ -2,49 +2,6 @@ import React, {ReactNode} from 'react';
 import GroupTbody from "./components/gtoup-tbody/group-tbody";
 import {ButtonProps} from "../button";
 
-
-// 定义数据项的类型
-export interface DataItem {
-    key: string;
-
-    [propName: string]: any;
-}
-
-// 定义列的类型
-export interface Column {
-    key: string;
-    title: string;
-    dataIndex: string;
-    render?: (data: any, record: DataItem) => ReactNode;
-}
-
-export interface ColumnGroup {
-    key: string;
-    title: string;
-    children: Column[]; //数组   集合
-}
-
-
-// 定义Table组件的属性类型
-export interface TableProps {
-    className?: string;// 样式类名
-    data?: DataItem[];// 数据
-    columns?: Column[];// 列数据
-    draggable?: boolean;// 是否可拖拽
-    onDdragAfter?: (data: DataItem[], column: Column[]) => void;// 拖拽后的回调
-    expandable?: Expandable;
-    tbodyClassName: string;
-    theadClassNmae: string;
-}
-
-// 定义Column组件的属性类型
-export interface ColumnProps {
-    title: string;
-    dataIndex: string;
-    render?: (data: any, record: DataItem) => ReactNode;
-    key?: string;
-}
-
 interface expandIconProps {
     expanded: boolean;
     record: number;
@@ -91,16 +48,62 @@ export interface GroupTbodyProps {
     activeTD: string | null;// 当前拖拽的列
 }
 
+// 定义数据项的类型
+export interface DataItem {
+    key: string;
+
+    [propName: string]: any;
+}
+
+// 定义列的类型
+export interface Column {
+    key: string | number;
+    title: string;
+    dataIndex: string;
+    render?: (data: any, record: DataItem) => ReactNode;
+}
+
+export interface ColumnGroup {
+    key: string | number;
+    title: string;
+    children: React.ReactNode<Column[]>;//数组   集合
+}
+
+
+// 定义Table组件的属性类型
+export interface TableProps {
+    className?: string;// 样式类名
+    data?: DataItem[];// 数据
+    columns?: Column[];// 列数据
+    draggable?: boolean;// 是否可拖拽
+    onDdragAfter?: (data: DataItem[], column: Column[]) => void;// 拖拽后的回调
+    expandable?: Expandable;
+    tbodyClassName: string;
+    theadClassNmae: string;
+}
+
+// 定义Column组件的属性类型
+export interface ColumnProps {
+    title: string;
+    dataIndex: string;
+    render?: (data: any, record: DataItem) => ReactNode;
+    key?: string;
+}
+
+
+declare namespace Table {
+    interface ColumnGroups extends ColumnGroup {
+        // 定义 ColumnGroup 的属性
+    }
+
+    interface Columns extends Column {
+        // 扩展 Column 接口，以支持 Table.ColumnGroup 和 Table.Column
+    }
+
+    export const ColumnGroup: React.FC<ColumnGroups>;
+    export const Column: React.FC<Columns>;
+}
 declare const Table: React.FC<TableProps>;
 
 export default Table;
 
-// declare module 'Table' {
-//     export function Table({data, columns}: TableProps): React.FC;
-//
-//     export function Column({title, dataIndex, render, key}: ColumnProps): null;
-//
-//     namespace Table {
-//         export const Column: typeof Column;
-//     }
-// }
