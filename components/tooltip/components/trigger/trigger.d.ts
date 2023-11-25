f19f81400cb27b3bc602f45c7f4af0d7aafb5c00
AyongNice <AyongNice@163.com>
2023-11-25 12:13:41 +0800

    1 修复placementType枚举语法

--- components/tooltip/components/trigger/trigger.d.ts
+++ components/tooltip/components/trigger/trigger.d.ts
@@ -1,7 +1,8 @@
 import React from 'react';
 
-interface placementType {
-    | 'topLeft'
+
+type placementType =
+    'topLeft'
     | 'top'
     | 'topRight'
     | 'bottomLeft'
@@ -13,19 +14,17 @@ interface placementType {
     | 'rightTop'
     | 'right'
     | 'rightBottom';
-}
-
 
 export interface TriggerWrapProps {
-	popupPlacement: placementType;
-	action: Array<'hover' | 'click'>;
-	popupTransitionName: string;
-	popupVisible?: boolean;
-	popup: React.ReactNode;
-	onPopupVisibleChange?: (visible: boolean) => void;
-	popupClassName?: string;
-	popupAlign?: object;
-	getPopupContainer?: () => HTMLElement;
-	onPopupAlign?: (domNode: HTMLElement, align: object) => void;
-	children: React.ReactNode;
+    popupPlacement: placementType;
+    action: Array<'hover' | 'click'>;
+    popupTransitionName: string;
+    popupVisible?: boolean;
+    popup: React.ReactNode;
+    onPopupVisibleChange?: (visible: boolean) => void;
+    popupClassName?: string;
+    popupAlign?: object;
+    getPopupContainer?: () => HTMLElement;
+    onPopupAlign?: (domNode: HTMLElement, align: object) => void;
+    children: React.ReactNode;
 }
