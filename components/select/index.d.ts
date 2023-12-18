90d5b34daa67fbe2096aa977d1c5fb6e38e149fd
AyongNice <AyongNice@163.com>
2023-12-18 18:44:34 +0800

    1 模块划分组件 select

--- components/select/index.d.ts
+++ components/select/index.d.ts
@@ -1,26 +1,27 @@
 import React from 'react';
 
 export interface Options {
-    value: string;
-    label: string;
-    disabled?: boolean;
-    selectedValues?: string[];
+  value: string;
+  label: string;
+  disabled?: boolean;
+  selectedValues?: string[];
 }
 
 export interface SelectProps {
-    className?: string;// 类名
-    defaultValue?: string | string[];// 默认值
-    style?: React.CSSProperties;// 样式
-    onChange?: (value: string | string[]) => void;// 选中值改变时的回调
-    options: Options[];// 选项数据
-    disabled?: boolean;// 是否禁用
-    clearable?: boolean;// 是否可清空
+  className?: string;// 类名
+  defaultValue?: string | string[];// 默认值
+  style?: React.CSSProperties;// 样式
+  onChange?: (value: string | string[]) => void;// 选中值改变时的回调
+  options: Options[];// 选项数据
+  disabled?: boolean;// 是否禁用
+  clearable?: boolean;// 是否可清空
+  search?: boolean;// 是否可搜索
 }
 
 export interface OptionsParma {
-    options: Options[];// 选项数据
-    onClick: Function;// 点击事件
-    selectedValues?: string[];// 选中值
+  options: Options[];// 选项数据
+  onClick: Function;// 点击事件
+  selectedValues?: string[];// 选中值
 }
 
 declare const Select: React.FC<SelectProps>;
--- components/select/index.tsx
+++ components/select/index.tsx
@@ -1,141 +1,170 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import selectStyle from './index.module.less';
 import Multiple from './components/multiple/index.tsx'
-import {Wrong, Downwleftfu} from "../icon/icon.ts";
+import {Wrongs, Under, Search} from "../icon/icon.ts";
 import {SelectProps} from "./index.d";
 import Option from "./components/option/index.tsx";
 
+
+const LeftIcon = ({clearable, showClearable, clearValue, search, isDropdownVisible, selectedValues}) => {
+  return (
+    <div className={Array.isArray(selectedValues) ? selectStyle.iconBox : selectStyle.selectBox}>
+
+      {!Array.isArray(selectedValues) && <div
+        className={isDropdownVisible ? `${selectStyle.onValue} ${selectStyle.selectValue}` : selectStyle.selectValue}>{selectedValues}</div>}
+
+      {showClearable ? <Wrongs onClick={clearValue} className={selectStyle.close}/> :
+        <Under className={`${selectStyle.rotateTransform} ${
+          isDropdownVisible ? selectStyle.rotate90 : ''
+        }`}/>}
+
+    </div>
+
+  )
+}
 const CustomSelect: React.FC<SelectProps> = ({
-                                                 defaultValue,
-                                                 onChange = () => {
-                                                 },
-                                                 options,
-                                                 style,
-                                                 disabled,
-                                                 clearable
+                                               defaultValue,
+                                               onChange = () => {
+                                               },
+                                               options,
+                                               style,
+                                               disabled,
+                                               clearable,
+                                               search
                                              }) => {
-        /** 搜索选项 **/
-        const [searchTerm, setSearchTerm] = useState<string>('');
-        /** 选择值 **/
-        const [selectedValues, setSelectedValues] = useState<string | string[]>(defaultValue as string);
-        /** 是否显示下拉框 **/
-        const [isDropdownVisible, setIsDropdownVisible] = useState<boolean>(false);
-        /** 是否显示清除按钮 **/
-        const [showClearable, setShowClearable] = useState<boolean>(false);
+    /** 搜索选项 **/
+    const [searchTerm, setSearchTerm] = useState<string>('');
+    /** 选择值 **/
+    const [selectedValues, setSelectedValues] = useState<string | string[]>(defaultValue as string);
+    /** 是否显示下拉框 **/
+    const [isDropdownVisible, setIsDropdownVisible] = useState<boolean>(false);
+    /** 是否显示清除按钮 **/
+    const [showClearable, setShowClearable] = useState<boolean>(false);
+    useEffect(() => {
 
-        /**
-         * 点击选项
-         * @param value
-         */
-        const handleOptionClick = (value: string) => {
-            setSelectedValues((prevValues: string | string[]) => {
-                if (Array.isArray(selectedValues)) {
-                    return prevValues.includes(value) ? prevValues.filter((v) => v !== value) : [...prevValues, value]
-                }
-                return value
-            });
-            setSearchTerm('');
-            if (!Array.isArray(selectedValues)) {
-                setIsDropdownVisible(false)
-            }
-            onChange(selectedValues);
-        };
+      console.log(clearable, Array.isArray(selectedValues))
+    }, [])
 
-        const handleSelectClick = (event) => {
-            event.stopPropagation();
-            console.log('handleSelectClick')
-            setIsDropdownVisible(!isDropdownVisible);
-        };
 
-        const handleInputChange = (e) => {
-            setSearchTerm(e.target.value);
-        };
-        const handleBlur = () => {
-            // setIsDropdownVisible(false);
-        };
-        /**
-         * 按下键盘 多选情况下从后面删除
-         * @param event
-         */
-        const handleKeyDown = (): void => {
-            if (!searchTerm.length) {
-                setSelectedValues(selectedValues.slice(0, selectedValues.length - 1));
-                onChange(selectedValues.slice(0, selectedValues.length - 1));
-            }
-
+    /**
+     * 点击选项
+     * @param value
+     */
+    const handleOptionClick = (value: string) => {
+      setSelectedValues((prevValues: string | string[]) => {
+        if (Array.isArray(selectedValues)) {
+          return prevValues.includes(value) ? prevValues.filter((v) => v !== value) : [...prevValues, value]
         }
+        return value
+      });
+      setSearchTerm('');
+      if (!Array.isArray(selectedValues)) {
+        setIsDropdownVisible(false)
+      }
+      onChange(selectedValues);
+    };
 
-        /**
-         * 获取select的className
-         */
-        const getWarpClassname = () => {
-            const disabledName: string = disabled ? selectStyle.disabled : '';
-            return isDropdownVisible ? `${selectStyle.customSelect} ${selectStyle.active} ${disabledName}` : `${selectStyle.customSelect} ${disabledName}`
-        }
-        /**
-         * 鼠标移入显示清除按钮
-         */
-        const onMouseEnter = () => {
-            if (clearable) setShowClearable(true);
-        }
+    const handleSelectClick = (event) => {
+      if (disabled) return;
+      event.stopPropagation();
+      console.log('handleSelectClick')
+      setIsDropdownVisible(!isDropdownVisible);
+    };
 
-        /**
-         * 清除多选选项
-         */
-        const clearValue = () => {
-            setSelectedValues([]);
-            onChange([]);
-        }
+    const handleInputChange = (e) => {
+      setSearchTerm(e.target.value);
+    };
+    const handleBlur = () => {
+      // setIsDropdownVisible(false);
+    };
+    /**
+     * 按下键盘 多选情况下从后面删除
+     * @param event
+     */
+    const handleKeyDown = (): void => {
+      if (!searchTerm.length) {
+        setSelectedValues(selectedValues.slice(0, selectedValues.length - 1));
+        onChange(selectedValues.slice(0, selectedValues.length - 1));
+      }
 
-        return (
-            <div style={style}
-                 className={getWarpClassname()}>
-                <main className={selectStyle.main}
-                      onMouseEnter={onMouseEnter}
-                      onMouseLeave={() => setShowClearable(false)}
-                      onClick={handleSelectClick}>
-                    {Array.isArray(selectedValues) ?
-                        <React.Fragment>
-                            <Multiple
-                                selectedValues={selectedValues}
-                                value={searchTerm}
-                                onKeyDown={handleKeyDown}
-                                handleOptionClick={handleOptionClick}
-                                onInputClick={handleSelectClick}
-                                onChange={handleInputChange}
-                                onFocus={() => setIsDropdownVisible(true)}
-                                onBlur={handleBlur}
-                            />
-                            <div className={selectStyle.iconBox}>
-                                {showClearable ? <Wrong onClick={clearValue} className={selectStyle.close}/> :
-                                    <Downwleftfu className={`${selectStyle.rotateTransform} ${
-                                        isDropdownVisible ? selectStyle.rotate90 : ''
-                                    }`}/>}
-
-                            </div>
-
-                        </React.Fragment>
-                        : <div className={selectStyle.selectBox}>
-                            <div
-                                className={isDropdownVisible ? `${selectStyle.onValue} ${selectStyle.selectValue}` : selectStyle.selectValue}>{selectedValues}</div>
-                            <Downwleftfu className={`${selectStyle.rotateTransform} ${
-                                isDropdownVisible ? selectStyle.rotate90 : ''
-                            }`}/>
-                        </div>}
-                </main>
-
-
-                {isDropdownVisible &&
-                   <ul className={selectStyle.dropdown}>
-                      <Option options={options}
-                              selectedValues={selectedValues}
-                              onClick={handleOptionClick}
-                      />
-
-                   </ul>}
-            </div>
-        );
     }
+
+    /**
+     * 获取select的className
+     */
+    const getWarpClassname = () => {
+      const disabledName: string = disabled ? selectStyle.disabled : '';
+      return isDropdownVisible ? `${selectStyle.customSelect} ${selectStyle.active} ${disabledName}` : `${selectStyle.customSelect} ${disabledName}`
+    }
+    /**
+     * 鼠标移入显示清除按钮
+     */
+    const onMouseEnter = () => {
+      if (clearable) setShowClearable(true);
+    }
+
+    /**
+     * 清除多选选项
+     */
+    const clearValue = () => {
+      setSelectedValues(Array.isArray(selectedValues) ? [] : '');
+      onChange(Array.isArray(selectedValues) ? [] : '');
+    }
+
+
+    return (
+      <div style={style}
+           className={getWarpClassname()}>
+        <main
+          style={{height: Array.isArray(selectedValues) ? '100%' : '30px'}}
+          className={selectStyle.main}
+          onMouseEnter={onMouseEnter}
+          onMouseLeave={() => setShowClearable(false)}
+          onClick={handleSelectClick}>
+          {Array.isArray(selectedValues) ?
+            <React.Fragment>
+              <Multiple
+                selectedValues={selectedValues}
+                value={searchTerm}
+                onKeyDown={handleKeyDown}
+                handleOptionClick={handleOptionClick}
+                onChange={handleInputChange}
+                onFocus={() => setIsDropdownVisible(true)}
+                onBlur={handleBlur}
+              />
+
+              <LeftIcon
+                search={search}
+                clearable={clearable}
+                clearValue={clearValue}
+                showClearable={showClearable}
+                selectedValues={selectedValues}
+                isDropdownVisible={isDropdownVisible}/>
+
+
+            </React.Fragment>
+            : <LeftIcon
+              search={search}
+              clearable={clearable}
+              clearValue={clearValue}
+              showClearable={showClearable}
+              selectedValues={selectedValues}
+              isDropdownVisible={isDropdownVisible}/>}
+        </main>
+
+
+        {isDropdownVisible &&
+          <ul className={selectStyle.dropdown}>
+            <Option options={options}
+                    selectedValues={selectedValues}
+                    onClick={handleOptionClick}
+            />
+
+          </ul>
+        }
+      </div>
+    );
+  }
 ;
 
 export default CustomSelect;
