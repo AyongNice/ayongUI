5849156d541f82eb17e302219d51391abe987b35
chenhuayu <1171193188@qq.com>
2023-12-27 15:32:25 +0800

    完善Input组件内参数，删除无用参数及样式

--- components/input/index.d.ts
+++ components/input/index.d.ts
@@ -1,19 +1,18 @@
-import React from 'react';
-
+import React, { ReactPropTypes } from 'react';
 export interface InputProps {
+    value: ReactPropTypes.string, // 默认值
     style?: React.CSSProperties; // 自定义样式
-    time?: number; // 防抖时间设置 0 为不防抖
-    href?: string; // 跳转连接
-    className?: string; // 自定义样式类名
-    children: React.ReactNode; // 按钮内容
-    disabled?: boolean; // 禁用
+    className?: ReactPropTypes.string; // 自定义样式类名
+    disabled?: ReactPropTypes.bool; // 禁用
     icon?: React.ReactElement; // icon图标
-    type?: 'primary' | 'default' | 'dashed'; // 按钮类型 利用样式实现
-    size?: 'large' | 'default' | 'small'; // 按钮大小 利用样式padding 实现
-    shape?: 'circle' | 'default' | 'round'; // 按钮形状 利用样式border-radius 实现
-    htmlType?: 'button' | 'submit' | 'reset'; // 原生button类型
-    placeholder?: string; // 占位符
-    onClick?: () => void; // 点击事件
+    size?: 'large' | 'normal' | 'small' // 输入框尺寸，nora
+    type?: 'text' | 'password' | 'textarea' // 按钮类型：文本、密码、富文本、数字
+    btnType?: 'primary' | 'default' | 'dashed'; // 按钮样式类型 利用样式实现
+    placeholder?: ReactPropTypes.string; // 占位符
+    onFocus?: ReactPropTypes.func; // 聚焦回调
+    onBlur?: ReactPropTypes.func; // 失焦回调
+    onKeyUp?: ReactPropTypes.func; // 键入回调
+    onChange: ReactPropTypes.func; // 值变动回调
 }
 
 
--- components/input/index.tsx
+++ components/input/index.tsx
@@ -1,34 +1,37 @@
-import styles from './index.module.less';
-import { useState } from 'react';
-import { InputProps } from './index';
+import { InputProps } from './index.d'
+import styles from './index.module.less'
 
 export default function Input(props: InputProps) {
-	const [isExpanded, setIsExpanded] = useState<boolean>(false);
-
-	const {
-		style = {},
-		type = 'default',
-		size = 'default',
-		shape = 'default',
-		children,
-		href = '',
-		htmlType = 'button',
-		className = '',
+  const {
+		value,
 		disabled,
-		text,
-		time = 0,
-		icon = '',
-		onClick = () => {},
-		placeholder = '请输入值',
-	} = props;
-	/**
-	 * 默认使用组件classname  参数className 覆盖默认样式
-	 * 参数className > 默认使用组件classname > 参数样式
-	 *
-	 */
-	const styleClassName: string = `${styles.ayongInput} ${className} ${
-		styles[size]
-	} ${styles[type]} ${styles[shape]}  ${disabled && styles.notAllowed} `;
+    type = 'text',
+    className = '',
+    size = 'normal',
+    onFocus = () => {},
+    onBlur = () => {},
+    onKeyUp = () => {},
+    onChange = () => {},
+    placeholder = '请输入值',
+  } = props
+  /**
+   * 默认使用组件classname  参数className 覆盖默认样式
+   * 参数className > 默认使用组件classname > 参数样式
+   *
+   */
+  const styleClassName= `${styles.ayongInput} ${className} ${styles[size]} 
+												${styles[type]} ${disabled && styles.notAllowed} `
 
-	return <input className={styleClassName} placeholder={placeholder}></input>;
+  return (
+    <input
+      type={type}
+      value={value}
+      onBlur={onBlur}
+      onKeyUp={onKeyUp}
+      onFocus={onFocus}
+      onChange={onChange}
+      placeholder={placeholder}
+      className={styleClassName}
+    ></input>
+  )
 }
