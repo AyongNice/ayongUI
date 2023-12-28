a6319dc5de8f774992f05eb1aa1b42b296c0943a
AyongNice <AyongNice@163.com>
2023-12-28 12:11:38 +0800

    1 全局颜色名字调整 不在以组件名字命名

--- components/table/components/unfold-button/unfold-button.tsx
+++ components/table/components/unfold-button/unfold-button.tsx
@@ -1,58 +1,64 @@
 import ConditionalRender from "../../../conditional-render/conditional-render.tsx";
-import { UnfoldTdProps} from "../../index.d";
+import {UnfoldTdProps} from "../../index.d";
 import table from "../../index.module.less";
 import unfold from './index.module.less'
-import Index from "../../../button/index.tsx";
+import Button from "../../../button/index.tsx";
 import React from "react";
 
 const UnfoldButton = ({
-                          index,
-                          expandable = {},
-                          ayonEexpandedRowKeys,
-                          toggleExpand = () => {
-                          }
+                        index,
+                        expandable = {},
+                        ayonEexpandedRowKeys,
+                        toggleExpand = () => {
+                        }
                       }: UnfoldTdProps) => {
-    const {expandedRowKeys, expandIcon, onExpandChange} = expandable;
+  const {expandedRowKeys, expandIcon, onExpandChange} = expandable;
 
-    /** 当前展开状态 **/
-    const stateExpandedRowKeys: boolean = ayonEexpandedRowKeys.includes(index);
+  /** 当前展开状态 **/
+  const stateExpandedRowKeys: boolean = ayonEexpandedRowKeys.includes(index);
 
-    const onClickExpand = (index: number) => {
-        toggleExpand(index);
-        onExpandChange(index, stateExpandedRowKeys);
-    }
+  const onClickExpand = (index: number) => {
+    toggleExpand(index);
+    onExpandChange(index, stateExpandedRowKeys);
+  }
 
-    return (
-        <ConditionalRender mode='if' show={(Array.isArray(expandedRowKeys))}>
-            <td>
-                {expandable?.expandedRowKeys && expandable?.expandedRowKeys.map((rowKeys: number | string, rowKeysIndex: number) => {
-                    return Number(rowKeys) === index ? (
-                        // 使用传递进来的 expandIcon
-                        expandIcon && typeof expandIcon === 'function' ?
-                            <React.Fragment key={rowKeysIndex}>
-                                {expandIcon({
-                                    record: index,
-                                    expanded: stateExpandedRowKeys,
-                                    onExpand: toggleExpand
-                                })}
-                            </React.Fragment>
-                            : (
-                                <Index
-                                    size="mini"
-                                    style={{width: '20px'}}
-                                    key={rowKeysIndex}
-                                    onClick={() => onClickExpand(index)}
-                                    className={`${table.unfold} ${unfold.rotate180} ${
-                                        stateExpandedRowKeys ? unfold.rotate : ''
-                                    }`}
-                                >
-                                    {stateExpandedRowKeys ? '-' : '+'}
-                                </Index>
-                            )
-                    ) : null;
+  return (
+    <ConditionalRender mode='if' show={(Array.isArray(expandedRowKeys))}>
+      <td>
+        {expandable?.expandedRowKeys && expandable?.expandedRowKeys.map((rowKeys: number | string, rowKeysIndex: number) => {
+          return Number(rowKeys) === index ? (
+            // 使用传递进来的 expandIcon
+            expandIcon && typeof expandIcon === 'function' ?
+              <React.Fragment key={rowKeysIndex}>
+                {expandIcon({
+                  record: index,
+                  expanded: stateExpandedRowKeys,
+                  onExpand: toggleExpand
                 })}
-            </td>
-        </ConditionalRender>
-    )
+              </React.Fragment>
+              : (
+                <Button
+                  size="mini"
+                  style={{
+                    width: '15px',
+                    height: '15px',
+                    lineHeight: '15px',
+                    textAlign: 'center',
+                    padding: 0,
+                  }}
+                  key={rowKeysIndex}
+                  onClick={() => onClickExpand(index)}
+                  className={`${table.unfold} ${unfold.rotate180} ${
+                    stateExpandedRowKeys ? unfold.rotate : ''
+                  }`}
+                >
+                  {stateExpandedRowKeys ? '-' : '+'}
+                </Button>
+              )
+          ) : null;
+        })}
+      </td>
+    </ConditionalRender>
+  )
 }
 export default UnfoldButton;
