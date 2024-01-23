5b9826b460718452b2fe305e8ce86f6d661afec0
jackson4220 <898814220@qq.com>
2024-01-23 19:09:12 +0800

    feat:1.input输入框初步完成 , 2.tooltip 文档补充

--- components/input/index.d.ts
+++ components/input/index.d.ts
@@ -1,20 +1,21 @@
 import React, { ReactPropTypes } from 'react';
 export interface InputProps {
-    value: ReactPropTypes.string, // 默认值
-    style?: React.CSSProperties; // 自定义样式
-    className?: ReactPropTypes.string; // 自定义样式类名
-    disabled?: ReactPropTypes.bool; // 禁用
-    icon?: React.ReactElement; // icon图标
-    size?: 'large' | 'normal' | 'small' // 输入框尺寸，nora
-    type?: 'text' | 'password' | 'textarea' // 按钮类型：文本、密码、富文本、数字
-    btnType?: 'primary' | 'default' | 'dashed'; // 按钮样式类型 利用样式实现
-    placeholder?: ReactPropTypes.string; // 占位符
-    onFocus?: ReactPropTypes.func; // 聚焦回调
-    onBlur?: ReactPropTypes.func; // 失焦回调
-    onKeyUp?: ReactPropTypes.func; // 键入回调
-    onChange: ReactPropTypes.func; // 值变动回调
+	value: ReactPropTypes.string; // 默认值
+	style?: React.CSSProperties; // 自定义样式
+	className?: ReactPropTypes.string; // 自定义样式类名
+	disabled?: ReactPropTypes.bool; // 禁用
+	icon?: React.ReactElement; // icon图标
+	size?: 'large' | 'normal' | 'small'; // 输入框尺寸，nora
+	type?: 'text' | 'password' | 'textarea'; // 按钮类型：文本、密码、富文本、数字
+	btnType?: 'primary' | 'default' | 'dashed'; // 按钮样式类型 利用样式实现
+	placeholder?: ReactPropTypes.string; // 占位符
+	onFocus?: ReactPropTypes.func; // 聚焦回调
+	onBlur?: ReactPropTypes.func; // 失焦回调
+	onKeyUp?: ReactPropTypes.func; // 键入回调
+	onChange: ReactPropTypes.func; // 值变动回调
+	prefix?: React.ReactElement | string; // 前缀
+	suffix?: React.ReactElement | string; // 后缀
 }
-
 
 declare const Button: React.FC<InputProps>;
 
--- components/input/index.tsx
+++ components/input/index.tsx
@@ -1,37 +1,158 @@
-import { InputProps } from './index.d'
-import styles from './index.module.less'
+import React, { useState, useRef, useEffect } from 'react';
+import { Wrongs } from '../icon/icon';
+import styles from './index.module.less';
 
-export default function Input(props: InputProps) {
+export default function Input(props) {
   const {
-		value,
-		disabled,
+    value,
+    disabled,
     type = 'text',
     className = '',
     size = 'normal',
     onFocus = () => {},
     onBlur = () => {},
     onKeyUp = () => {},
-    onChange = () => {},
+    onChange = (e) => {
+      setValue(e.target.value);
+    },
     placeholder = '请输入值',
-  } = props
-  /**
-   * 默认使用组件classname  参数className 覆盖默认样式
-   * 参数className > 默认使用组件classname > 参数样式
-   *
-   */
-  const styleClassName= `${styles.ayongInput} ${className} ${styles[size]} 
-												${styles[type]} ${disabled && styles.notAllowed} `
+    prefix = null,
+    suffix = null,
+    clerabled = true,
+  } = props;
+
+  const [valueInner, setValue] = useState(value);
+
+  const styleClassName = `${styles.ayongInput} ${className} ${styles[size]} 
+    ${styles[type]} ${disabled && styles.notAllowed}`;
+
+  const prefixRef = useRef(null);
+  const suffixRef = useRef(null);
+  const clearRef = useRef(null);
+
+  const compensate = 5;
+  const [paddingLeft,setPdl] = useState('5px');
+  const [paddingRight,setPdr] = useState('5px');
+
+  
+  const [rightPlace, setRightPlace] = useState('5px');
+
+  const [reset,setReset] = useState(false);
+
+  useEffect(() => {
+    if (prefixRef.current) {
+        setPdl(prefix?prefixRef.current?.offsetWidth + compensate + 'px':'5px');
+    }
+  }, [prefixRef.current ]);
+
+  useEffect(() => {
+      if (suffixRef.current&&!clearRef.current) {
+        if (type=='number') {
+            const res = suffixRef.current?.offsetWidth + 4 + 'px';
+            setPdr(res);
+            console.log(res);
+        }else{
+            setPdr('5px');
+        } 
+      }else if(suffixRef.current&&clearRef.current){
+        if (type=='number') {
+            const clearWidth = clearRef.current?.offsetWidth+4;
+            const suffixWidth = suffixRef.current?.offsetWidth+4;
+            const res1 = clearWidth + suffixWidth + 'px';
+            setPdr(res1);
+            const res2 = suffixWidth + 'px';
+            setRightPlace(res2);
+        }else{
+            setPdr('5px');
+        }
+      }else{
+        //有删除,没有后插槽
+        if (type=='number') {
+            const res =  20 + 'px';
+            setPdr(res);
+        }else{
+            setPdr('5px');
+        }
+        /* if (clerabled) {
+            if (type=='number') {
+                const res =  20 + 'px';
+                setPdr(res);
+            }else{
+                setPdr('5px');
+            }
+        }else{
+            setPdr('5px');
+        } */
+      }
+  }, [suffixRef.current,clearRef.current,reset]);
+
+  const handleClear = () => {
+    setValue('');
+    setReset(!reset);
+    setTimeout(() => {
+        setReset(!reset);
+    }, 0);
+    // setPdr('5px');
+  };
+
+
+ /*  useEffect(() => {
+      if (clearRef.current) {
+        if (type=='number') {
+            const res = clearRef.current?.offsetWidth + 8 + 'px';
+            setRightPlace(res);
+        }else{
+            setRightPlace('5px');
+        }
+    }
+  }, [clearRef.current]); */
 
   return (
-    <input
-      type={type}
-      value={value}
-      onBlur={onBlur}
-      onKeyUp={onKeyUp}
-      onFocus={onFocus}
-      onChange={onChange}
-      placeholder={placeholder}
-      className={styleClassName}
-    ></input>
-  )
+    <div style={{ position: 'relative', maxWidth: 'max-content',boxSizing:'border-box',animation:'all 0.5s ' }}>
+      {prefix && (
+        <span
+          ref={prefixRef}
+          style={{
+            position: 'absolute',
+            left: '5px',
+            top: '50%',
+            transform: 'translateY(-50%)',
+          }}
+        >
+          {prefix}
+        </span>
+      )}
+      <input
+        style={{ paddingLeft, paddingRight,boxSizing:'border-box',animation:'all 0.5s ' }}
+        type={type}
+        value={valueInner}
+        onBlur={onBlur}
+        onKeyUp={onKeyUp}
+        onFocus={onFocus}
+        onChange={onChange}
+        placeholder={placeholder}
+        className={styleClassName}
+      />
+      {clerabled&&valueInner && (
+        <span
+        ref={clearRef}
+          style={{
+            position: 'absolute',
+            right: rightPlace,
+            top: '50%',
+            transform: 'translateY(-50%)',
+          }}
+          onClick={handleClear}
+        >
+          <Wrongs />
+        </span>
+      )}
+      {suffix && <span ref={suffixRef}  style={{
+            position: 'absolute',
+            right: '5px',
+            top: '50%',
+            transform: 'translateY(-50%)',
+          }}>{suffix}</span>}
+    </div>
+  );
 }
